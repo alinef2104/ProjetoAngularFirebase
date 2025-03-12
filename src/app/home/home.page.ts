afc1563a9ddaa37069e22d71e8fec33dc43cbd76
alinef2104 <160281953+alinef2104@users.noreply.github.com>
2025-03-12 19:25:31 +0000

    rede social

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,7 +10,41 @@ import { Router } from '@angular/router';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  perfil: any = {
+    foto: null,
+    nome: null,
+    profissao: null,
+    nome_usuario: null,
+    idioma: null,
+    localidade: null,
+    data_inicio: null,
+    biografia: null,
+    estatisticas: {
+      curtidas: 0,
+      seguindo: 0,
+      amigos:0
+    },
+    postagens: [
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ3bEhwkj1DdbOYY4og0PHCnTmg73ApP0dEA&s',
+        nome:'Billie Elish',
+        nome_usuario:'@billieelish',
+        texto: 'lorem',
+        data: '12/03/2025'
+      },
+
+      {
+        foto: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ3bEhwkj1DdbOYY4og0PHCnTmg73ApP0dEA&s',
+        nome:'Billie Elish',
+        nome_usuario:'@billieelish',
+        texto: 'outro post',
+        data: '12/03/2025'
+      }
+    ]
+
+  }
 
   constructor( ){ }
 
